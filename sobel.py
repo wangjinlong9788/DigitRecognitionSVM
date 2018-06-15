b89ec5ce342f09896ec2953b367c0816f4ebe70f
Wilson <wangjinlong98889@gmail.com>
2018-06-14 20:50:15 -0500

    Update sobel.py

--- sobel.py
+++ sobel.py
@@ -7,7 +7,7 @@ img = cv2.imread("car.jpg", 0)
 x = cv2.Sobel(img,cv2.CV_16S,1,0)  
 y = cv2.Sobel(img,cv2.CV_16S,0,1)  
   
-absX = cv2.convertScaleAbs(x)   # 转回uint8  
+absX = cv2.convertScaleAbs(x)   # uint8  
 absY = cv2.convertScaleAbs(y)  
   
 dst = cv2.addWeighted(absX,0.5,absY,0.5,0)  
